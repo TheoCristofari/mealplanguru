29dd6be4e76575b52d1cf8a5b6e5d6b82e9b3ccf
Theo Cristofari <270315517+TheoCristofari@users.noreply.github.com>
2026-04-29 15:26:42 +0100

    Revert "Use ingredient previews on recipe cards"
    This reverts commit c928b3c911ab52f9895c33e74af4247c618b0256.

--- script.js
+++ script.js
@@ -1284,20 +1284,6 @@ function isRecipePlanned(recipe) {
   return Object.values(mealPlan).includes(recipe.name);
 }
 
-function formatIngredientPreview(ingredients) {
-  const previewItems = getIngredientLines(ingredients)
-    .slice(0, 4)
-    .map((ingredient) => parseIngredient(ingredient).item.replace(/\s*\([^)]*\)/g, ""))
-    .filter(Boolean);
-
-  if (previewItems.length === 0) {
-    return "Ingredients";
-  }
-
-  const preview = previewItems.join(", ");
-  return `${preview.charAt(0).toUpperCase()}${preview.slice(1)}...`;
-}
-
 function createRecipeCard(recipe) {
   const card = document.createElement("article");
   card.className = "recipe-card recipe-card-filled";
@@ -1347,7 +1333,10 @@ function createRecipeCard(recipe) {
 
   const ingredientSummary = document.createElement("summary");
   ingredientSummary.innerHTML = `
-    <span class="recipe-ingredients-preview">${formatIngredientPreview(recipe.ingredients)}</span>
+    <span>Ingredients</span>
+    <svg class="recipe-ingredients-chevron" viewBox="0 0 24 24" fill="none" aria-hidden="true">
+      <path d="M6 9l6 6 6-6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+    </svg>
   `;
 
   const ingredientList = document.createElement("ul");
